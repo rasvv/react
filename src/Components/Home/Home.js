8046904f1df4ad54ee81584aa0bf18856010a4e0
rasvv <rasvv@mail.ru>
2021-07-28 17:42:11 +0300

    Домашнее задание к уроку №5

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,25 @@
+// import { func } from 'prop-types'
+import { Grid } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+import React from 'react'
+import Message from '../Message/Message'
+import { USER } from '../Constants'
+
+function Home() {
+	return (
+		<main>
+			<Grid container >
+				<Grid item xs={12}>
+					< Message user={USER.me}/>
+				</Grid>
+				<Grid item xs={12}>
+					<Link	className="speaklink" to="/chats">
+						Перейти к беседам
+					</Link> 		
+				</Grid>
+			</Grid>
+		</main>
+	)
+}
+
+export default Home
