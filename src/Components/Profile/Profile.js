9a018035024c5a7e4a68eef569879f6c515154fe
rasvv <rasvv@mail.ru>
2021-07-28 17:38:13 +0300

    ДЗ№5

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,37 +1,79 @@
-// import {Callback} from 'react'
+import React from 'react'
+import { FormControlLabel, Checkbox, Input } from '@material-ui/core'
 import {useSelector, useDispatch} from 'react-redux'
-// import {useCallback} from 'react'
-// import {showName} from state
+import { toggleShowName, toggleShowAge, toggleShowNameInput, changeName } from '../../actions/profile'
 
 
-function Profile() {
-	const { showName, name } = useSelector((state) => state.profile)
+
+export default function Profile() {
+	const { showName, showNameInput, showAge, name, age } = useSelector((state) => state.profile)
 	const dispatch = useDispatch()
 
-	const toggleShowName = (showName) => {
-		return {showName} ? 'false' : 'true' 
+	const setSName = (event) => {
+		dispatch(changeName(event.target.value))
 	}
 
 	const setShowName = (event) => {
-		// const new
-		console.log(event.target.checked)
 		dispatch(toggleShowName(event.target.checked))
 	}
 
+	const setShowAge = (event) => {
+		dispatch(toggleShowAge(event.target.checked))
+	}
+
+	const setShowNameInput = (event) => {
+		dispatch(toggleShowNameInput(event.target.checked))
+	}
 
 	return (
 		<div>
 			<h3> Страница профиля</h3>
-			<input 
-				type='checkbox'
-				checked={showName}
-				value={showName}
-				onChange={setShowName}
+			<FormControlLabel
+				control= {
+					<Checkbox
+						checked={showName}
+						onChange={setShowName}
+						name='ChB'
+						color='primary'
+					/>
+				}
+				label='Показать имя'
 			/>
-			<span>ShowName</span>
-			{/* {showName && <div>{ name }</div>} */}
+			{showName && 
+				<div>
+					<b>{ name }</b>
+					<br/>
+						<Checkbox
+							checked={showNameInput}
+							onChange={setShowNameInput}
+							name='ChBName'
+							color='primary'
+						/>
+					Изменить имя
+				</div>
+			}
+			{showNameInput && 
+				<div>
+					<Input
+						type='text'
+						value={name}
+						onChange={setSName}
+					/>
+				</div>
+			}
+			<br/>
+			<FormControlLabel
+				control= {
+					<Checkbox
+						checked={showAge}
+						onChange={setShowAge}
+						name='ChBAge'
+						color='primary'
+					/>
+				}
+				label='Показать возраст'
+			/>
+			{showAge && <div><b>{ age }</b></div>}
 		</div>
 	)
 }
-
-export default Profile
