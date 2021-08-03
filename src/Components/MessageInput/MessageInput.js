cd8c027712d62635f43660e1733bb829485fe83a
rasvv@mail.ru <rasvv>
2021-08-03 11:42:36 +0300

    Домашнее задание к уроку №6

---
+++ src/Components/MessageInput/MessageInput.js
@@ -0,0 +1,57 @@
+import "./MessageInput.sass"
+import { useState } from 'react'
+import {Button, TextField, Grid} from '@material-ui/core'
+import { USER } from '../Constants'
+
+function MessageInput(props) {
+	const [value, setValue] = useState('');
+
+	const onchange = (e) => {
+		setValue(e.target.value)
+	}
+
+	const onkeydown = (e) => {
+		if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+			handlerOnClick()
+		}
+	}
+
+	const handlerOnClick = () => {
+		props.updateData(value, `${USER.me}`)
+		setValue('')	
+	}
+
+	return (
+		<div className="messageinput">
+			<Grid container>
+				<Grid item xs={8}>
+					<TextField 
+						id="standard-search" 
+						label={ props.placeholder } 
+						value={value} 
+						onKeyDown={onkeydown} 
+						onChange={onchange} 
+						autoFocus
+					/>					
+				</Grid>
+				<Grid item xs={4}>
+					<Button 
+						variant="contained" 
+						color="primary"
+						size="large"
+						onClick={handlerOnClick}
+					>
+						Отправить
+					</Button>					
+				</Grid>
+			</Grid>
+
+
+		</div>
+	)
+}
+
+export default MessageInput
+
+
+
